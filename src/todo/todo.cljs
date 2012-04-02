2bea4f9831729522e0d96a9692b93da47e977259
emerge <emerge260@gmail.com>
2012-04-02 20:13:43 +0800

    1st

--- src/todo/todo.cljs
+++ src/todo/todo.cljs
@@ -1,23 +1,29 @@
 (ns todo.core)
-(def jquery (js* "$"))
+(def $ (js* "$"))
 (def debug? true)
 
-(defn- debug-log [& vs]
+(defn debug-log [& vs]
   (when debug?
     (.log js/console (apply str vs))))
 
+(defn- store []
+  [])
+
+(defn- render [app]
+  (do
+    (.append ($ "#wrapper") "<p>1 Hello World!</p>")
+    (.append ($ "#wrapper") "<p>2 Hello World!</p>")
+    ))
 
 (defn- create-app []
-    {:ENTER_KEY 13
-    }
-  )
-
+  (let [app
+    {
+      :todos (store)
+    }]
+    (render app)))
 
 (defn init [& args]
-  (do
-    (let [app create-app])
-      (js/alert "Hello from ClojureScript!"))
-    )
+  (.ready ($ js/document) create-app))
 
 
 
