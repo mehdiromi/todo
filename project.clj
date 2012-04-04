3d42e1c95c2e2b2db59ffdaacd82c7500a425f3a
emerge <emerge260@gmail.com>
2012-04-04 23:22:56 +0800

    up

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject todo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :plugins [[lein-cljsbuild "0.1.3"]]
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [
+                [org.clojure/clojure "1.3.0"]
                 [crate "0.1.0-SNAPSHOT"]
-                [jayq "0.1.0-alpha2"]]
+                [jayq "0.1.0-alpha2"]
+                ]
   :cljsbuild {
     :builds [{
         :source-path "src"
--- src/todo/todo.cljs
+++ src/todo/todo.cljs
@@ -8,27 +8,39 @@
   (when debug?
     (.log js/console (apply str vs))))
 
-(defn- store []
-  [])
+(defn uuid
+  "returns a type 4 random UUID: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+  []
+  (let [r (repeatedly 30 (fn [] (.toString (rand-int 16) 16)))]
+    (apply str (concat (take 8 r) ["-"]
+                       (take 4 (drop 8 r)) ["-4"]
+                       (take 3 (drop 12 r)) ["-"]
+                       [(.toString  (bit-or 0x8 (bit-and 0x3 (rand-int 15))) 16)]
+                       (take 3 (drop 15 r)) ["-"]
+                       (take 12 (drop 18 r))))))
+
+(defn- load-todos []
+  [ {:name "hello world1"
+     :id (uuid)
+     :done false}
+    {:name "hello world2"
+     :id (uuid)
+     :done false}])
+
+(defn- render-todo [todos]
+  (doseq [todo todos]
+    (do
+      (.append ($ "#wrapper") (crate/html
+        [:li {:class "todo"}
+          [:div {:class "inner"}
+            [:div {:class "name"}
+              (todo :name)]]])))))
 
 (defn- render [app]
-  (do
-    (.append ($ "#wrapper") (crate/html
-      [:li {:class "todo"}
-        [:div {:class "inner"}
-          [:div {:class "name"}
-            "Tap me"]]]))
-    (.append ($ "#wrapper") (crate/html
-      [:li {:class "todo"}
-        [:div {:class "inner"}
-          [:div {:class "name"}
-            "Tap me"]]]))
-    ))
+  (render-todo (app :todos)))
 
 (defn- create-app []
-  (let [app {
-      :todos (store)
-    }]
+  (let [app {:todos (load-todos)}]
     (render app)))
 
 (defn init [& args]
